c2bb4e31f72bdc159f7b6bbb55bae81b6d32fa20
Naohiro OHTA <naohta@gmail.com>
2013-11-06 17:30:03 +0900

    Works!

--- study_zbar/study_zbar/ViewController.m
+++ study_zbar/study_zbar/ViewController.m
@@ -3,10 +3,6 @@
 #import "AppDelegate.h"
 #import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()
-
-@end
-
 @implementation ViewController
 
 - (void)viewDidLoad
@@ -22,31 +18,12 @@
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
 
 - (IBAction)scanButtonTapped:(id)sender {
     NSLog(@"%s",__func__);
     [self.readerView.scanner setSymbology:ZBAR_I25 config:ZBAR_CFG_ENABLE to:0];
     [self.readerView start];
-}
-
-- (void)imagePickerController:(UIImagePickerController *)reader didFinishPickingMediaWithInfo:(NSDictionary *)info
-{
-    NSLog(@"%s",__func__);
-    id<NSFastEnumeration> results = [info objectForKey:ZBarReaderControllerResults];
-    ZBarSymbol *symbol = nil; 
-    NSMutableArray *ary = [@[] mutableCopy];
-    for (symbol in results) [ary addObject:symbol.data];
-    NSLog(@"%@",ary);
-    NSMutableString *barcodeText = [@"" mutableCopy];
-    for (NSString *s in ary) {
-        [barcodeText appendString:s];
-        [barcodeText appendString:@"\n"];
-    }
-    self.resultText.text = barcodeText;
-    [reader dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void) readerView: (ZBarReaderView*) view didReadSymbols: (ZBarSymbolSet*) syms fromImage: (UIImage*) img
